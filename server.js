3824828cef47ed01fa9f020446cf64c03269adfd
annaconway <azc5184@rit.edu>
2021-04-20 17:01:14 -0400

    First commit

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const unirest = require("unirest");
 const API_KEY = "546f0157-d581-4695-8240-44497135f2b6";
 
 app.get('/api/associations/:word', (req, res) => {
-	const word = req.params.word;
-	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	
+    const word = req.params.word;
+	
+    const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
 	.then(response => {
 		const results = response.body.response[0].items || []; // grab array of results
 		console.log(`Num results=${results.length}`);
@@ -16,6 +18,7 @@ app.get('/api/associations/:word', (req, res) => {
 		console.log(`error=${error}`);
 		res.json({status:"Error", message: `${error}`});
 	});
+
 });
 
 app.listen(port, () => {
